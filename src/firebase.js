50fc996c11b4c6d8f12b2cfcf2a42b520719055b
Austin Webb <stevenaustinwebb@gmail.com>
2019-06-09 11:40:31 -0400

    removed keys

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from 'firebase';
 
 var firebaseConfig = {
-    apiKey: "AIzaSyBA8daPWPyplDvJCds4wW1sHeZUTqgnylI",
-    authDomain: "auth-demo-819ef.firebaseapp.com",
-    databaseURL: "https://auth-demo-819ef.firebaseio.com",
-    projectId: "auth-demo-819ef",
-    storageBucket: "auth-demo-819ef.appspot.com",
-    messagingSenderId: "535564515100",
-    appId: "1:535564515100:web:56b27def6fae5104"
+    apiKey: ,
+    authDomain: ,
+    databaseURL: ,
+    projectId: ,
+    storageBucket: ,
+    messagingSenderId: ,
+    appId: 
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
